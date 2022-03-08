54c16d7487ee4fb7bb94275558b51ac2837f3ea4
Bryan Thomas <49354825+bryanjtc@users.noreply.github.com>
2022-03-08 14:48:18 +0000

    Added enzyme adapter for react 17

--- src/__test__/components/Product.test.js
+++ src/__test__/components/Product.test.js
@@ -1,9 +1,12 @@
 import 'jsdom-global/register';
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import Enzyme, { mount, shallow } from 'enzyme';
 import ProviderMock from '../../__mocks__/ProviderMock';
 import ProductMock from '../../__mocks__/ProductMock';
 import Product from '../../components/Product';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('<Product/>', () => {
   test('Render del componente Product', () => {
